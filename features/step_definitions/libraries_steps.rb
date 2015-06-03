06d7cac8f3e2c8f26faec490152481134fbbcc3d
dathi <dathi@tma.com.vn>
2015-06-03 23:20:54 +0700

    fix bug 'new' view, implement second Given and When in definition steps

--- features/step_definitions/libraries_steps.rb
+++ features/step_definitions/libraries_steps.rb
@@ -2,12 +2,12 @@ Given(/^I go to the new book page$/) do
   visit new_book_path
 end
 
-Given(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |label, value|
+  fill_in label, :with => value
 end
 
-When(/^I press "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I press "(.*?)"$/) do |create|
+  click_button create
 end
 
 Then(/^I should be on the book list page$/) do
