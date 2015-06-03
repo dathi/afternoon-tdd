31050a9528d3932c9654f432dc7e9bc7ee599c18
dathi <dathi@tma.com.vn>
2015-06-03 22:58:37 +0700

    update view, then update controller spec, then update controller, then create empty model Book to pass spec

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
   describe "#new" do
-  	it "should be successful" do
+  	before do
   	  get :new
+  	end
+  	it "should be successful" do
   	  expect(response).to be_success
+  	end
+  	it "should create a book object" do
+  	  expect(assigns(:book)).not_to be_nil
   	end
   end
 end
